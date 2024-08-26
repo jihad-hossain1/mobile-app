bc0b003f78c40dd7235646d1f4e6714fa0b59312
jihad hossain <jihadkhan934@gmail.com>
2024-08-27 01:16:40 +0600

    tab icon add

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,27 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { Slot, Stack } from 'expo-router'
+import React, { useEffect } from 'react'
+import { Slot, SplashScreen, Stack } from 'expo-router';
+import { useFonts } from 'expo-font';
 
+
+SplashScreen.preventAutoHideAsync();
 
 const RootLayout = () => {
-  return (<Stack >
+  const [fontsLoaded,error] = useFonts({
+    "SpaceMono-Regular": require('../assets/fonts/SpaceMono-Regular.ttf'),
+  })
+
+  useEffect(()=>{
+    if(error) throw error;
+    if(!fontsLoaded) SplashScreen.hideAsync();
+  },[fontsLoaded,error]);
+
+  if(!fontsLoaded && !error) return null;
+  return (
+  <Stack >
     <Stack.Screen name='index' options={{ headerShown: false }} />
-  </Stack>)
+  </Stack>
+  )
 }
 
 export default RootLayout
--- app/index.tsx
+++ app/index.tsx
@@ -7,9 +7,9 @@ export default function App(){
 
     return (
         <View className='flex-1 items-center justify-center bg-white'>
-            <Text>Hello World!</Text>
+            <Text className='text-3xl font-bold'>Hello World!</Text>
             <StatusBar style="auto" />
-            <Link href={'/profile'} className='text-blue-500'>Go to Profile</Link>
+            <Link href={'/home'} className='bg-blue-600 text-gray-50 px-4 py-2 rounded  border border-gray-400 shadow-[0_1px_2px_rgba(0,0,0,0.1)]'>Home</Link>
         </View>
     )
 }
